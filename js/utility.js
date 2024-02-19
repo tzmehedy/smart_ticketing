d480204e2fd860576ed756858cede08e0d670ee3
unknown <mehedytouhidurzaman@gmail.com>
2024-02-19 19:07:55 +0600

    total seat count completed

--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,10 @@
 function addInTable(id){
+    const totalAvailableSeat = document.getElementById("totalAvailableSeat");
+    const totalAvailableSeatValue = totalAvailableSeat.innerText;
+    const remainingSeatInNumber = parseInt(totalAvailableSeatValue);
+   
+    const remainingSeat = remainingSeatInNumber - 1;
+    totalAvailableSeat.innerText = remainingSeat;
     const pressButton = document.getElementById(id);
     pressButton.classList.remove("bg-gray-300");
     pressButton.classList.add("bg-[#1DD100]");
