308866f8c332fa2ed3bf9ee11b252c3d8a6ff4ee
unknown <mehedytouhidurzaman@gmail.com>
2024-02-19 18:54:25 +0600

    click button green completed

--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,7 @@
 function addInTable(id){
+    const pressButton = document.getElementById(id);
+    pressButton.classList.remove("bg-gray-300");
+    pressButton.classList.add("bg-[#1DD100]");
     const appendTableBody = document.getElementById("appendTableBody");
 
     const newTr = document.createElement('tr');
